25a5d54cb80bf40628b823982694d5ade325f51b
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2018-11-04 02:38:04 +0530

    Update template.cpp

--- C++/template.cpp
+++ C++/template.cpp
@@ -54,11 +54,16 @@ const int dyhorse[] = {1, -1, 2, -2, 2, -2, 1, -1};
 
 template<typename T> inline void maxi(T &x, T y){ x = max(x,y); }
 template<typename T> inline void mini(T &x, T y){ x = min(x,y); }
-template<typename T> inline T Pow(T base, T exp) { LL result = 1; while (exp) {if (exp&1)result *= base; exp >>= 1; base *= base;} return result; }
-template<typename T> inline T Mulmod(T a, T b, T c) { LL x=0, y=a%c; while (b>0) {if (b&1) x = (x+y)%c; y = (y<<1)%c; b >>= 1;} return x; }
-template<typename T> inline T Pow(T base, T exp, T mod) {LL x=1, y=base; while (exp>0){if (exp&1) x = Mulmod(x, y, mod); y = Mulmod(y, y, mod); exp >>= 1;} return x;}
+ttemplate<typename T> inline void add(T &x, T y){ x+= y; if(x >= MOD) x-= MOD;}
+template<typename T> inline void sub(T &x, T y){ x-=y; if (x < 0) x += MOD;}
+template<typename T> inline T mul(T a, T b) { LL x =(long long) a*b; x%= MOD; return x; }
+template<typename T> inline T power(T base, T exp) { LL result = 1; while (exp) {if (exp&1)result *= base; exp >>= 1; base *= base;} return result; }
+template<typename T> inline T power(T a, T b, T mod) {  int res = 1;while (b > 0) { if (b & 1) {res = mul(res, a); }a = mul(a, a);b >>= 1;} return res;}
 template<typename T> inline bool isPrime(T n) { if(n<=1) return false; if(n<=3) return true; if (n%2 == 0 || n%3 == 0) return false; for(LL i=5; i*i <= n; i+=6){ if ((n%i == 0)||(n%(i+2)==0)) {return false;}}return true;}
-
+bool is_digit(char x){ return x>= '0' && x<= '9';}
+bool is_upper(char x){ return x>= 'A' && x<= 'Z';}
+bool is_lower(char x){ return x>= 'a' && x<= 'z';}
+bool is_char(char x){ return x == '!' || x=='@' || x=='#' || x=='$' || x=='%' || x=='&' ;}
 
 int main() {
   IOS;
