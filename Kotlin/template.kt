9dd55ff3b264b3a437473ef6c39b332a44eae9ac
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-09-05 22:44:57 +0530

    Update template.kt

--- Kotlin/template.kt
+++ Kotlin/template.kt
@@ -1,29 +1,28 @@
-
 import java.util.*
- 
+
 val MX = 1000005
 val MOD = 998244353
 val SZ = 1 shl 19 // shl == <<
 val INF = (1e18).toLong()
- 
+
 fun add(a: Int, b: Int) = (a + b) % MOD
 fun sub(a: Int, b: Int) = (a - b + MOD) % MOD
 fun mul(a: Int, b: Int) = ((a.toLong() * b) % MOD).toInt()
- 
+
 fun next() = readLine()!!
 fun nextInt() = next().toInt()
 fun nextLong() = next().toLong()
 fun nextInts() = next().split(" ").map { it.toInt() }
 fun nextLongs() = next().split(" ").map { it.toLong() }
 
-fun solve(){
-  
+fun solve() {
+    
 }
 
 
 fun main(args: Array<String>) {
     val t = nextInt()
-    for (i in 0..t-1) {
+    for (i in 0 until t) {
         solve()
     }
 }
