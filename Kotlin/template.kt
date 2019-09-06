200509ec64f8072e27faee6f2dacfc81e39baf4c
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-09-07 00:32:23 +0530

    Update template.kt

--- Kotlin/template.kt
+++ Kotlin/template.kt
@@ -1,8 +1,18 @@
 import java.util.*
+import kotlin.collections.*
+
+typealias PII = Pair<Int, Int>
+typealias PLL = Pair<Long, Long>
+typealias PPII = Pair<Int, PII>
+typealias APII = ArrayList<PII>
+typealias APLL = ArrayList<PLL>
+typealias AAPII = ArrayList<ArrayList<PII>>
+typealias AAPLL = ArrayList<ArrayList<PLL>>
 
 val MX = 1000005
 val MOD = 998244353
 val SZ = 1 shl 19 // shl == <<
+val LG = 1 shr MOD
 val INF = (1e18).toLong()
 
 fun add(a: Int, b: Int) = (a + b) % MOD
@@ -15,14 +25,14 @@ fun nextLong() = next().toLong()
 fun nextInts() = next().split(" ").map { it.toInt() }
 fun nextLongs() = next().split(" ").map { it.toLong() }
 
-fun solve() {
-    
+fun solve(testCase: Int) {
+
 }
 
 
 fun main(args: Array<String>) {
     val t = nextInt()
-    for (i in 0 until t) {
-        solve()
+    for (i in 1..t) {
+        solve(i)
     }
 }
