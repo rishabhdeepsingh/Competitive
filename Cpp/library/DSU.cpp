6c7c3db089cf519839b37f7bd3c6ae38cd06f1de
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-09-25 04:16:54 +0530

    Create DSU.cpp

---
+++ Cpp/library/DSU.cpp
@@ -0,0 +1,36 @@
+class DSU {
+    int n;
+  public:
+    vector<int> par, rank;
+    DSU(int _n) : n(_n) {
+        par.resize(n);
+        rank.resize(n);
+        for (int i = 0; i < n; ++i) {
+            par[i] = i;
+            rank[i] = 1;
+        }
+    }
+    int Root(int x) {
+        return x == par[x] ? x : par[x] = Root(par[x]);
+    }
+    bool Unite(int x, int y) {
+        x = Root(x);
+        y = Root(y);
+        if(x == y) return false;
+        if(rank[x] > rank[y]){
+            par[y] = x;
+            rank[x] += rank[y];
+        }else{
+            par[x] = y;
+            rank[y] += rank[x];
+        }
+        return true;
+    }
+    int Count(){
+        int cnt = 0;
+        for (int i = 0; i < n; ++i) {
+            if(Root(i) == i) cnt++;
+        }
+        return cnt;
+    }
+};
