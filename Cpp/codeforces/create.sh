64227d56086d7c249fb84a619b4a1e603f3881bf
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-12-15 16:47:09 +0530

    Update create.sh

--- Cpp/codeforces/create.sh
+++ Cpp/codeforces/create.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# Remove all Directories
-rm -R -- */
-
 read -a tasks -p "Please give the tasks of tasks: "
 # If empty take a default contest 
 if [[ ${#tasks[@]} -eq 0 ]] ; then
     echo "Taking defaults a,b,c,d,e "
-    tasks=(a b c d e f)
+    tasks=(a b c d e)
 fi
 
-printf "%s\n" "${tasks[@]}" | xargs -I {} sh -c 'mkdir -p $1 && cp template.cpp $1/sol.cpp' sh {}
- 
+echo ${tasks[@]}
+
+for task in ${tasks[@]}
+do
+    if [ -d "$task" ]; then
+        echo "Task $task already exists; continuing"
+        continue
+    fi
+    mkdir $task
+    cp template.cpp $task/sol.cpp 
+    cp runall.sh gen.cpp test.sh time.sh $task/
+ done
+
+ echo "Done Creating files"
