21988a04577b82476f1ff5d858b194880eb072c6
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2020-04-24 04:08:50 +0530

    Create forest.hpp

---
+++ Cpp/library/ds/forest.hpp
@@ -0,0 +1,25 @@
+//
+// Created by White Knife on 14/04/20.
+//
+
+#include "graph.hpp"
+
+template <typename T>
+class forest : public graph<T> {
+public:
+  using graph<T>::edges;
+  using graph<T>::g;
+  using graph<T>::n;
+
+  forest(int _n) : graph<T>(_n) {}
+
+  int add(int from, int to, T cost = 1) {
+    assert(0 <= from && from < n && 0 <= to && to < n);
+    int id = (int) edges.size();
+    assert(id < n - 1);
+    g[from].push_back(id);
+    g[to].push_back(id);
+    edges.push_back({from, to, cost});
+    return id;
+  }
+};
