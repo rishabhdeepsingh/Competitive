7f67c7dc7e31ac7626172968e5db2a5de135571e
rishabhdeepsingh <rishabhdeepsingh98@gmail.com>
2020-08-26 02:39:41 +0530

    update numbers.hpp

--- Cpp/library/number/numbers.hpp
+++ Cpp/library/number/numbers.hpp
@@ -1,54 +1,7 @@
 #pragma once
 
-#include "../IO.hpp"
-
-const long long MOD7 = 1000000007;
-const long long MOD9 = 1000000009;
-const long long MODF = 998244353;
-
-long long MOD = 4294967296ll;
-
-template <class T>
-inline T add(T &x, T y) {
-  x += y;
-  if (x >= MOD) x -= MOD;
-  return x;
-}
-
-template <class T>
-inline T sub(T &x, T y) {
-  x -= y;
-  if (x < 0) x += MOD;
-  return x;
-}
-
-template <typename T >
-T mul(T x, T y) { x %= MOD, y %= MOD; return (x * (long long) y) % MOD; }
-
-template <typename T, typename U>
-T power(T base, U exp, T mod) {
-  T res = 1;
-  while (exp > 0) {
-    if (exp & 1) { res = mul(res, base); }
-    base = mul(base, base);
-    exp >>= 1;
-  }
-  return res % mod;
-}
-
-template <typename T>
-T inverse(T a, T m) {
-  T u = 0, v = 1;
-  while(a != 0){
-    T t = m/a;
-    m -= t*a;
-    swap(a, m);
-    u -= t*v;
-    swap(u, v);
-  }
-  assert(m == 1);
-  return u;
-}
+#include "../general.hpp"
+#include "../collections/arr.hpp"
 
 template <typename T>
 T gcd(T a, T b) {
@@ -67,7 +20,7 @@ T lcm(T a, T b) {
 }
 
 template <typename T>
-T power(const T &a, long long b) {
+T power(const T& a, long long b) {
   if (b == 0) {
     return 1;
   }
