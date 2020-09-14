7b0e6b4b167c1ceacaf8417beebc07f0c2d67283
rishabhdeepsingh <rishabhdeepsingh98@gmail.com>
2020-09-15 01:22:29 +0530

    added numbers headers

---
+++ Cpp/library/number/factorizer.hpp
@@ -0,0 +1,288 @@
+#pragma once
+#include "Mint.hpp"
+
+
+namespace factorizer {
+
+template <typename T>
+struct FactorizerVarMod { static T value; };
+template <typename T>
+T FactorizerVarMod<T>::value;
+
+template <typename T>
+bool IsPrime(T n, const vector<T>& bases) {
+  if (n < 2) {
+    return false;
+  }
+  vector<T> small_primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+  for (const T& x : small_primes) {
+    if (n % x == 0) {
+      return n == x;
+    }
+  }
+  if (n < 31 * 31) {
+    return true;
+  }
+  int s = 0;
+  T d = n - 1;
+  while ((d & 1) == 0) {
+    d >>= 1;
+    s++;
+  }
+  FactorizerVarMod<T>::value = n;
+  for (const T& a : bases) {
+    if (a % n == 0) {
+      continue;
+    }
+    Modular<FactorizerVarMod<T>> cur = a;
+    cur = power(cur, d);
+    if (cur == 1) {
+      continue;
+    }
+    bool witness = true;
+    for (int r = 0; r < s; r++) {
+      if (cur == n - 1) {
+        witness = false;
+        break;
+      }
+      cur *= cur;
+    }
+    if (witness) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool IsPrime(int64_t n) {
+  return IsPrime(n, {2, 325, 9375, 28178, 450775, 9780504, 1795265022});
+}
+
+bool IsPrime(int32_t n) {
+  return IsPrime(n, {2, 7, 61});
+}
+
+// but if you really need uint64_t version...
+/*
+bool IsPrime(uint64_t n) {
+  if (n < 2) {
+    return false;
+  }
+  vector<uint32_t> small_primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+  for (uint32_t x : small_primes) {
+    if (n == x) {
+      return true;
+    }
+    if (n % x == 0) {
+      return false;
+    }
+  }
+  if (n < 31 * 31) {
+    return true;
+  }
+  uint32_t s = __builtin_ctzll(n - 1);
+  uint64_t d = (n - 1) >> s;
+  function<bool(uint64_t)> witness = [&n, &s, &d](uint64_t a) {
+    uint64_t cur = 1, p = d;
+    while (p > 0) {
+      if (p & 1) {
+        cur = (__uint128_t) cur * a % n;
+      }
+      a = (__uint128_t) a * a % n;
+      p >>= 1;
+    }
+    if (cur == 1) {
+      return false;
+    }
+    for (uint32_t r = 0; r < s; r++) {
+      if (cur == n - 1) {
+        return false;
+      }
+      cur = (__uint128_t) cur * cur % n;
+    }
+    return true;
+  };
+  vector<uint64_t> bases_64bit = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+  for (uint64_t a : bases_64bit) {
+    if (a % n == 0) {
+      return true;
+    }
+    if (witness(a)) {
+      return false;
+    }
+  }
+  return true;
+}
+*/
+
+vector<int> least = {0, 1};
+vector<int> primes;
+int precalculated = 1;
+
+void RunLinearSieve(int n) {
+  n = max(n, 1);
+  least.assign(n + 1, 0);
+  primes.clear();
+  for (int i = 2; i <= n; i++) {
+    if (least[i] == 0) {
+      least[i] = i;
+      primes.push_back(i);
+    }
+    for (int x : primes) {
+      if (x > least[i] || i * x > n) {
+        break;
+      }
+      least[i * x] = x;
+    }
+  }
+  precalculated = n;
+}
+
+void RunSlowSieve(int n) {
+  n = max(n, 1);
+  least.assign(n + 1, 0);
+  for (int i = 2; i * i <= n; i++) {
+    if (least[i] == 0) {
+      for (int j = i * i; j <= n; j += i) {
+        if (least[j] == 0) {
+          least[j] = i;
+        }
+      }
+    }
+  }
+  primes.clear();
+  for (int i = 2; i <= n; i++) {
+    if (least[i] == 0) {
+      least[i] = i;
+      primes.push_back(i);
+    }
+  }
+  precalculated = n;
+}
+
+void RunSieve(int n) {
+  RunLinearSieve(n);
+}
+
+template <typename T>
+vector<pair<T, int>> MergeFactors(const vector<pair<T, int>>& a, const vector<pair<T, int>>& b) {
+  vector<pair<T, int>> c;
+  int i = 0;
+  int j = 0;
+  while (i < (int) a.size() || j < (int) b.size()) {
+    if (i < (int) a.size() && j < (int) b.size() && a[i].first == b[j].first) {
+      c.emplace_back(a[i].first, a[i].second + b[j].second);
+      ++i;
+      ++j;
+      continue;
+    }
+    if (j == (int) b.size() || (i < (int) a.size() && a[i].first < b[j].first)) {
+      c.push_back(a[i++]);
+    } else {
+      c.push_back(b[j++]);
+    }
+  }
+  return c;
+}
+
+template <typename T>
+vector<pair<T, int>> RhoC(const T& n, const T& c) {
+  if (n <= 1) {
+    return {};
+  }
+  if ((n & 1) == 0) {
+    return MergeFactors({{2, 1}}, RhoC(n / 2, c));
+  }
+  if (IsPrime(n)) {
+    return {{n, 1}};
+  }
+  FactorizerVarMod<T>::value = n;
+  Modular<FactorizerVarMod<T>> x = 2;
+  Modular<FactorizerVarMod<T>> saved = 2;
+  T power = 1;
+  T lam = 1;
+  while (true) {
+    x = x * x + c;
+    T g = __gcd((x - saved)(), n);
+    if (g != 1) {
+      return MergeFactors(RhoC(g, c + 1), RhoC(n / g, c + 1));
+    }
+    if (power == lam) {
+      saved = x;
+      power <<= 1;
+      lam = 0;
+    }
+    lam++;
+  }
+  return {};
+}
+
+template <typename T>
+vector<pair<T, int>> Rho(const T& n) {
+  return RhoC(n, static_cast<T>(1));
+}
+
+template <typename T>
+vector<pair<T, int>> Factorize(T x) {
+  if (x <= 1) {
+    return {};
+  }
+  if (x <= precalculated) {
+    vector<pair<T, int>> ret;
+    while (x > 1) {
+      if (!ret.empty() && ret.back().first == least[x]) {
+        ret.back().second++;
+      } else {
+        ret.emplace_back(least[x], 1);
+      }
+      x /= least[x];
+    }
+    return ret;
+  }
+  if (x <= static_cast<int64_t>(precalculated) * precalculated) {
+    vector<pair<T, int>> ret;
+    if (!IsPrime(x)) {
+      for (T i : primes) {
+        T t = x / i;
+        if (i > t) {
+          break;
+        }
+        if (x == t * i) {
+          int cnt = 0;
+          while (x % i == 0) {
+            x /= i;
+            cnt++;
+          }
+          ret.emplace_back(i, cnt);
+          if (IsPrime(x)) {
+            break;
+          }
+        }
+      }
+    }
+    if (x > 1) {
+      ret.emplace_back(x, 1);
+    }
+    return ret;
+  }
+  return Rho(x);
+}
+
+template <typename T>
+vector<T> BuildDivisorsFromFactors(const vector<pair<T, int>>& factors) {
+  vector<T> divisors = {1};
+  for (auto& p : factors) {
+    int sz = (int) divisors.size();
+    for (int i = 0; i < sz; i++) {
+      T cur = divisors[i];
+      for (int j = 0; j < p.second; j++) {
+        cur *= p.first;
+        divisors.push_back(cur);
+      }
+    }
+  }
+  sort(divisors.begin(), divisors.end());
+  return divisors;
+}
+
+}  // namespace factorizer
---
+++ Cpp/library/number/primitive.hpp
@@ -0,0 +1,60 @@
+#pragma once
+#include "factorizer.hpp"
+
+template <typename T>
+struct PrimitiveVarMod { static T value; };
+template <typename T>
+T PrimitiveVarMod<T>::value;
+
+template <typename T, class F>
+T GetPrimitiveRoot(const T& modulo, const F& factorize) {
+  if (modulo <= 0) {
+    return -1;
+  }
+  if (modulo == 1 || modulo == 2 || modulo == 4) {
+    return modulo - 1;
+  }
+  vector<pair<T, int>> modulo_factors = factorize(modulo);
+  if (modulo_factors[0].first == 2 && (modulo_factors[0].second != 1 || modulo_factors.size() != 2)) {
+    return -1;
+  }
+  if (modulo_factors[0].first != 2 && modulo_factors.size() != 1) {
+    return -1;
+  }
+  set<T> phi_factors;
+  T phi = modulo;
+  for (auto& d : modulo_factors) {
+    phi = phi / d.first * (d.first - 1);
+    if (d.second > 1) {
+      phi_factors.insert(d.first);
+    }
+    for (auto& e : factorize(d.first - 1)) {
+      phi_factors.insert(e.first);
+    }
+  }
+  PrimitiveVarMod<T>::value = modulo;
+  Modular<PrimitiveVarMod<T>> gen = 2;
+  while (gen != 0) {
+    if (power(gen, phi) != 1) {
+      continue;
+    }
+    bool ok = true;
+    for (auto& p : phi_factors) {
+      if (power(gen, phi / p) == 1) {
+        ok = false;
+        break;
+      }
+    }
+    if (ok) {
+      return gen();
+    }
+    gen++;
+  }
+  assert(false);
+  return -1;
+}
+
+template <typename T>
+T GetPrimitiveRoot(const T& modulo) {
+  return GetPrimitiveRoot(modulo, factorizer::Factorize<T>);
+}
