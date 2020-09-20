faec9e6b0c4f81a0bfc53965c0c685e1d2510174
rishabhdeepsingh <rishabhdeepsingh98@gmail.com>
2020-09-21 00:51:57 +0530

    update DSU

--- Cpp/library/collections/DSU.hpp
+++ Cpp/library/collections/DSU.hpp
@@ -4,36 +4,29 @@
 struct DSU {
   int n;
   std::vector<int> par;
-  std::vector<long long> sz;
+  std::vector<int> rank;
   
   explicit DSU(int _n) : n(_n) {
     par.resize(n);
-    sz.resize(n);
+    rank.resize(n);
     for (int i = 0; i < n; ++i) {
       par[i] = i;
-      sz[i] = 1;
+      rank[i] = 1;
     }
   }
   
-  int Root(int x) {
-    return par[x] = (x == par[x]) ? x : Root(par[x]);
+  int root(int x) {
+    return par[x] = (x == par[x]) ? x : root(par[x]);
   }
   
-  bool Unite(int x, int y) {
-    x = Root(x);
-    y = Root(y);
+  bool unite(int x, int y) {
+    x = root(x);
+    y = root(y);
     if (x == y) return true;
-    if (sz[x] < sz[y]) swap(x, y);
+    if (rank[x] < rank[y]) swap(x, y);
     par[y] = x;
-    sz[x] += sz[y];
+//    if (rank[x] == rank[y]) rank[x]++; // Rank
+    rank[x] += rank[y]; // Size use any one.
     return false;
-  }
-  
-  int Count() {
-    int cnt = 0;
-    for (int i = 0; i < n; ++i) {
-      if (Root(i) == i) cnt++;
-    }
-    return cnt;
   }
 };
