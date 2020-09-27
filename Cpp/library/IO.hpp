aaf38f06e8b4fe2cf7395ba44f106fdeead4e302
rishabhdeepsingh <rishabhdeepsingh98@gmail.com>
2020-09-28 02:32:52 +0530

    update IO.hpp

--- Cpp/library/IO.hpp
+++ Cpp/library/IO.hpp
@@ -13,35 +13,17 @@ using namespace __gnu_pbds;
 using namespace __gnu_cxx;
 using namespace std;
 
-// @formatter:off
-#define F               first
-#define S               second
-#define all(x)          (x).begin(), (x).end()
-#define sz(x)           (long long) (x).size()
-
-using uint = unsigned int;
 using ll = long long;
-using ull = unsigned long long;
 using ld = long double;
-//using li = __int128;
-//using uli = unsigned __int128;
-using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 using vi = vector<int>;
-using vb = vector<bool>;
 
-using vvi = vector<vi>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
-using vpii = vector<pii>;
-using vpll = vector<pll>;
 template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const ld PI = acos(-1);
 
-template<typename T> inline T mini(T& x, T y) { return x = min(x, y); }
-template<typename T> inline T maxi(T& x, T y) { return x = max(x, y); }
-template <typename T> T rand(T x, T y) { return rand() % (y - x + 1) + x; }
+template <typename T> inline T mini(T& x, T y) { return x = min(x, y); }
+template <typename T> inline T maxi(T& x, T y) { return x = max(x, y); }
 mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
 template <typename T, typename U>
 std::ostream& operator<<(std::ostream& stream, const pair<T, U>& p) {
@@ -54,7 +36,7 @@ std::ostream& operator<<(std::ostream& out, const vector<T>& vec) {
 }
 template <typename T>
 std::istream& operator>>(std::istream& in, vector<T>& vec) {
-  for(auto& x: vec) in >> x;
+  for (auto& x: vec) in >> x;
   return in;
 }
 // @formatter:on
