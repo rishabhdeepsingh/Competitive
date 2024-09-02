724871659d6a84750c338a662870f93ab379fbe5
Rishabhdeep Singh <rishabhdeepsingh98@gmail.com>
2024-09-02 11:32:52 +0000

    Remove DSU as we can use atcoder dsu library.

--- Cpp/codeforces/template/sol.cpp
+++ Cpp/codeforces/template/sol.cpp
@@ -3,6 +3,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// @formatter:off
+#define sim template <typename T
+#define ris return *this
+#define dor > debug &operator<<
+#define eni(x) \
+  sim > typename enable_if<sizeof dud<T>(0) x 1, debug &>::type operator<<(T i)
+sim > struct rge {
+  T b, e;
+};
+sim > rge<T> rang(T i, T j) { return rge<T>{i, j}; }
+sim > auto dud(T *x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+  ~debug() { cerr << endl; }
+  eni(!=) {
+    cerr << boolalpha << i;
+    ris;
+  }
+  eni(==) { ris << rang((i).begin(), (i).end()); }
+  sim, typename U dor(pair<U, T> d) {
+    ris << "(" << d.first << ", " << d.second << ")";
+  }
+  sim dor(rge<T> d) {
+    *this << "[";
+    for (auto it = d.b; it != d.e; ++it) *this << ", " + 2 * (it == d.b) << *it;
+    ris << "]";
+  }
+#else
+  sim dor(const T &) { ris; }
+#endif
+};
+#define pr(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// @formatter:on
+
 using ll = long long;
 using i128 = __int128_t;
 using ld = long double;
@@ -11,26 +46,30 @@ using vi = vector<int>;
 
 const ld PI = acos(-1);
 
-template<typename T, typename U>
-[[maybe_unused]] inline bool chmax(T &a, U b) { return a < b && (a = b, true); }
-template<typename T, typename U>
-[[maybe_unused]] inline bool chmin(T &a, U b) { return a > b && (a = b, true); }
+template <typename T, typename U>
+[[maybe_unused]] inline bool chmax(T &a, U b) {
+  return a < b && (a = b, true);
+}
+template <typename T, typename U>
+[[maybe_unused]] inline bool chmin(T &a, U b) {
+  return a > b && (a = b, true);
+}
 mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
-template<typename T, typename U>
+template <typename T, typename U>
 std::ostream &operator<<(std::ostream &stream, const pair<T, U> &p) {
   return stream << p.first << " " << p.second;
 }
-template<typename T1, typename U>
+template <typename T1, typename U>
 std::istream &operator>>(std::istream &is, std::pair<T1, U> &p) {
   is >> p.first >> p.second;
   return is;
 }
-template<typename T>
+template <typename T>
 std::istream &operator>>(std::istream &in, std::vector<T> &vec) {
   for (auto &x : vec) in >> x;
   return in;
 }
-template<typename T>
+template <typename T>
 std::ostream &operator<<(std::ostream &out, std::vector<T> &vec) {
   for (std::size_t i = 0; i < vec.size(); ++i) {
     out << vec[i];
@@ -39,9 +78,7 @@ std::ostream &operator<<(std::ostream &out, std::vector<T> &vec) {
   return out;
 }
 
-auto solve() {
-  
-}
+auto solve() {}
 
 int main() {
   ios::sync_with_stdio(false);
